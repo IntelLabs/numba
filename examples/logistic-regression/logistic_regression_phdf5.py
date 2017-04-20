f764d6e6576866f6a258aab4325d49abc10d64d4
Ehsan Totoni <ehsan.totoni@intel.com>
2017-04-19 19:04:51 -0700

    phdf5 lr

---
+++ examples/logistic-regression/logistic_regression_phdf5.py
@@ -0,0 +1,34 @@
+import numba
+import numpy as np
+import h5py
+import argparse
+import time
+
+@numba.njit(parallel=True)
+def logistic_regression(file_name, iterations):
+    f = h5py.File(file_name, "r")
+    X = f['points'].value.astype(np.float64)
+    Y = f['responses'].value.astype(np.float64)
+    D = X.shape[1]
+    w = 2.0*np.ones(D)-1.3
+    for i in range(iterations):
+        w -= np.dot(((1.0 / (1.0 + np.exp(-Y * np.dot(X,w))) - 1.0) * Y),X)
+    return w
+
+def main():
+    parser = argparse.ArgumentParser(description='Logistic Regression.')
+    parser.add_argument('--file', dest='file', type=str, default="lr.hdf5")
+    parser.add_argument('--iterations', dest='iterations', type=int, default=30)
+    args = parser.parse_args()
+
+    file_name = args.file
+    iterations = args.iterations
+    
+    t = time.time()
+    w = logistic_regression(file_name, iterations)
+    selftimed = time.time()-t
+    print("SELFTIMED ", selftimed)
+    print("result: ", w)
+
+if __name__ == '__main__':
+    main()
