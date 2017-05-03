403b2b97221955d310ab3485377e6fc05d23734d
Ehsan Totoni <ehsan.totoni@intel.com>
2017-05-02 18:33:02 -0700

    gen dist init WIP

--- numba/distributed.py
+++ numba/distributed.py
@@ -14,6 +14,7 @@ from numba.parfor import Parfor
 import numpy as np
 
 import h5py
+from mpi4py import MPI
 
 from enum import Enum
 class Distribution(Enum):
@@ -25,15 +26,19 @@ _dist_analysis_result = namedtuple('dist_analysis_result', 'array_dists,parfor_d
 
 class DistributedPass(object):
     """analyze program and transfrom to distributed"""
-    def __init__(self, func_ir, typemap):
+    def __init__(self, func_ir, typemap, calltypes):
         self.func_ir = func_ir
         self.typemap = typemap
+        self.calltypes = calltypes
+        self._rank_var = None # will be set in run
 
     def run(self):
         dprint_func_ir(self.func_ir, "starting distributed pass")
         dist_analysis = self._analyze_dist()
         if config.DEBUG_ARRAY_OPT==1:
             print("distributions: ", dist_analysis)
+        self._run_dist_pass()
+        dprint_func_ir(self.func_ir, "after distributed pass")
 
     def _analyze_dist(self):
         topo_order = find_topo_order(self.func_ir.blocks)
@@ -70,6 +75,36 @@ class DistributedPass(object):
         if parfor.id not in parfor_dists:
             parfor_dists[parfor.id] = Distribution.OneD
         return
+
+    def _run_dist_pass(self):
+        topo_order = find_topo_order(self.func_ir.blocks)
+        first_block = self.func_ir.blocks[topo_order[0]]
+        # set scope and loc of generated code to the first variable in block
+        scope = first_block.body[0].target.scope
+        loc = first_block.body[0].target.loc
+        dist_inits = self._get_dist_inits(scope, loc)
+        first_block.body = dist_inits+first_block.body
+
+    def _get_dist_inits(self, scope, loc):
+        out = []
+        # g_mpi_var = Global(numba.distributed)
+        g_mpi_var = ir.Var(scope, mk_unique_var("$distributed_g_var"), loc)
+        self.typemap[g_mpi_var.name] = types.misc.Module(numba.distributed)
+        g_mpi = ir.Global('distributed', numba.distributed, loc)
+        g_mpi_assign = ir.Assign(g_mpi, g_mpi_var, loc)
+        # attr call: rank_attr = getattr(g_mpi_var, get_rank)
+        rank_attr_call = ir.Expr.getattr(g_mpi_var, "get_rank", loc)
+        rank_attr_var = ir.Var(scope, mk_unique_var("$get_rank_attr"), loc)
+        # typemap =
+        rank_attr_assign = ir.Assign(rank_attr_call, rank_attr_var, loc)
+        # rank_var = numba.distributed.get_rank()
+        rank_var = ir.Var(scope, mk_unique_var("$rank"), loc)
+        rank_call = ir.Expr.call(rank_attr_var, [], (), loc)
+        rank_assign = ir.Assign(rank_call, rank_var, loc)
+        self._rank_var = rank_var
+        out += [g_mpi_assign, rank_attr_assign, rank_assign]
+
+        return out
 
     def _run_assign(self):
         return
